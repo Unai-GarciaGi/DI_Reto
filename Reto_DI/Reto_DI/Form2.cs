4513b03dabfd78840e7c8a21b8a94136d1e4fe04
Unai García Giralde <unaibaraka@outlook.es>
2022-01-31 13:17:37 +0100

    Primer ejercicio del form 2

--- Reto_DI/Reto_DI/Form2.cs
+++ Reto_DI/Reto_DI/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,29 @@ namespace Reto_DI
         public Form2()
         {
             InitializeComponent();
+        }
+        private string sql;
+        private int cont;
+        private MiConexion Conexion = new MiConexion();
+        //SqlCommand cmd = new SqlCommand(sql, Conexion.pConexion);
+        //Conexion.AbrirConexion();
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            sql = "SELECT * FROM Vendedores";
+            SqlCommand cmd = new SqlCommand(sql, Conexion.pConexion);
+            Conexion.AbrirConexion();
+            SqlDataAdapter das = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            das.Fill(ds, "tabla");
+            dataGridView1.DataSource = ds.Tables[0];
+            /*
+             Otra manera de hacerlo según StackOverflow
+            DataTable dt = new DataTable();
+            das.Fill(dt);
+            dataGridView1.ItemsSource= dt.DefaultView;
+             */
+            Conexion.CerrarConexion();
         }
     }
 }
