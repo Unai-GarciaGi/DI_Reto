173c1fb5d6fa1e32e516bc40431dc7c3a3f6b4f5
Unai <unai.garciagi@elorrieta-errekamari.com>
2022-02-02 19:12:18 +0100

    Form 2 casi terminado

--- Reto_DI/Reto_DI/Form2.Designer.cs
+++ Reto_DI/Reto_DI/Form2.Designer.cs
@@ -29,10 +29,29 @@ namespace Reto_DI
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.btnCargar = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.empresaDataSet = new Reto_DI.EmpresaDataSet();
+            this.vendedoresBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.vendedoresTableAdapter = new Reto_DI.EmpresaDataSetTableAdapters.VendedoresTableAdapter();
+            this.btnAddX = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnBorrarVend = new System.Windows.Forms.Button();
+            this.txtBorrar = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.btnFila = new System.Windows.Forms.Button();
+            this.txtFila = new System.Windows.Forms.TextBox();
+            this.txtNombreBuscar = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.empresaDataSet1 = new Reto_DI.EmpresaDataSet();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empresaDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vendedoresBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empresaDataSet1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -42,6 +61,7 @@ namespace Reto_DI
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(776, 213);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // btnGuardar
             // 
@@ -63,11 +83,136 @@ namespace Reto_DI
             this.btnCargar.UseVisualStyleBackColor = true;
             this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
             // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(13, 71);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(94, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // empresaDataSet
+            // 
+            this.empresaDataSet.DataSetName = "EmpresaDataSet";
+            this.empresaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // vendedoresBindingSource
+            // 
+            this.vendedoresBindingSource.DataMember = "Vendedores";
+            this.vendedoresBindingSource.DataSource = this.empresaDataSet;
+            // 
+            // vendedoresTableAdapter
+            // 
+            this.vendedoresTableAdapter.ClearBeforeFill = true;
+            // 
+            // btnAddX
+            // 
+            this.btnAddX.Location = new System.Drawing.Point(13, 101);
+            this.btnAddX.Name = "btnAddX";
+            this.btnAddX.Size = new System.Drawing.Size(94, 23);
+            this.btnAddX.TabIndex = 4;
+            this.btnAddX.Text = "Add X";
+            this.btnAddX.UseVisualStyleBackColor = true;
+            this.btnAddX.Click += new System.EventHandler(this.btnAddX_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(13, 131);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(94, 23);
+            this.btnBorrar.TabIndex = 5;
+            this.btnBorrar.Text = "Borrar char";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnBorrarVend
+            // 
+            this.btnBorrarVend.Location = new System.Drawing.Point(411, 13);
+            this.btnBorrarVend.Name = "btnBorrarVend";
+            this.btnBorrarVend.Size = new System.Drawing.Size(95, 23);
+            this.btnBorrarVend.TabIndex = 6;
+            this.btnBorrarVend.Text = "Borrar Vendedor";
+            this.btnBorrarVend.UseVisualStyleBackColor = true;
+            // 
+            // txtBorrar
+            // 
+            this.txtBorrar.Location = new System.Drawing.Point(513, 15);
+            this.txtBorrar.Name = "txtBorrar";
+            this.txtBorrar.Size = new System.Drawing.Size(178, 20);
+            this.txtBorrar.TabIndex = 7;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(411, 44);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(154, 20);
+            this.txtNombre.TabIndex = 8;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(571, 45);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(154, 20);
+            this.txtSalario.TabIndex = 9;
+            // 
+            // btnFila
+            // 
+            this.btnFila.Location = new System.Drawing.Point(411, 71);
+            this.btnFila.Name = "btnFila";
+            this.btnFila.Size = new System.Drawing.Size(95, 23);
+            this.btnFila.TabIndex = 10;
+            this.btnFila.Text = "Ir a fila";
+            this.btnFila.UseVisualStyleBackColor = true;
+            this.btnFila.Click += new System.EventHandler(this.btnFila_Click);
+            // 
+            // txtFila
+            // 
+            this.txtFila.Location = new System.Drawing.Point(513, 73);
+            this.txtFila.Name = "txtFila";
+            this.txtFila.Size = new System.Drawing.Size(26, 20);
+            this.txtFila.TabIndex = 11;
+            // 
+            // txtNombreBuscar
+            // 
+            this.txtNombreBuscar.Location = new System.Drawing.Point(513, 101);
+            this.txtNombreBuscar.Name = "txtNombreBuscar";
+            this.txtNombreBuscar.Size = new System.Drawing.Size(212, 20);
+            this.txtNombreBuscar.TabIndex = 12;
+            this.txtNombreBuscar.TextChanged += new System.EventHandler(this.txtNombreBuscar_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(411, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Buscar Nombre";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // empresaDataSet1
+            // 
+            this.empresaDataSet1.DataSetName = "EmpresaDataSet";
+            this.empresaDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNombreBuscar);
+            this.Controls.Add(this.txtFila);
+            this.Controls.Add(this.btnFila);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtBorrar);
+            this.Controls.Add(this.btnBorrarVend);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnAddX);
+            this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnCargar);
             this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.dataGridView1);
@@ -75,7 +220,11 @@ namespace Reto_DI
             this.Text = "Form2";
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empresaDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vendedoresBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.empresaDataSet1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -84,5 +233,20 @@ namespace Reto_DI
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnGuardar;
         private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnUpdate;
+        private EmpresaDataSet empresaDataSet;
+        private System.Windows.Forms.BindingSource vendedoresBindingSource;
+        private EmpresaDataSetTableAdapters.VendedoresTableAdapter vendedoresTableAdapter;
+        private System.Windows.Forms.Button btnAddX;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnBorrarVend;
+        private System.Windows.Forms.TextBox txtBorrar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Button btnFila;
+        private System.Windows.Forms.TextBox txtFila;
+        private System.Windows.Forms.TextBox txtNombreBuscar;
+        private System.Windows.Forms.Button button1;
+        private EmpresaDataSet empresaDataSet1;
     }
 }
--- Reto_DI/Reto_DI/Form2.cs
+++ Reto_DI/Reto_DI/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
@@ -23,10 +24,15 @@ namespace Reto_DI
         private MiConexion Conexion = new MiConexion();
         //SqlCommand cmd = new SqlCommand(sql, Conexion.pConexion);
         //Conexion.AbrirConexion();
+        private Boolean cambioBuscar;
+        private int posicion;
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            sql = "SELECT * FROM Vendedores";
+            cambioBuscar = true;
+            // TODO: esta línea de código carga datos en la tabla 'empresaDataSet.Vendedores' Puede moverla o quitarla según sea necesario.
+            //this.vendedoresTableAdapter.Fill(this.empresaDataSet.Vendedores);
+            sql = "SELECT CodVend, NombVen, DirecVen, Telefono, Salario FROM Vendedores";
             SqlCommand cmd = new SqlCommand(sql, Conexion.pConexion);
             Conexion.AbrirConexion();
             SqlDataAdapter das = new SqlDataAdapter(cmd);
@@ -44,13 +50,8 @@ namespace Reto_DI
 
         private void btnGuardar_click(object sender, EventArgs e)
         {
-            sql = "SELECT * FROM Vendedores";
-            SqlCommand cmd = new SqlCommand(sql, Conexion.pConexion);
-            Conexion.AbrirConexion();
-            SqlDataAdapter das = new SqlDataAdapter(cmd);
-            DataSet ds = new DataSet();
-            das.Fill(ds);
-            ds.WriteXml(fichXML);
+            DataTable dt = (DataTable) dataGridView1.DataSource;
+            dt.WriteXml(fichXML);
         }
 
         private void btnCargar_Click(object sender, EventArgs e)
@@ -58,6 +59,137 @@ namespace Reto_DI
             DataSet ds = new DataSet();
             ds.ReadXml(fichXML);
             dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //NO SE COMO FUNCIONA ESTO TENGO QUE PREGUNTAR
+            OleDbConnection conn = null;
+            conn = new OleDbConnection("Server=LOCALHOST;DataBase=Empresa;Integrated Security=true");
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+            //Creo el comando
+            adapter.UpdateCommand = new OleDbCommand("UPDATE Vendedores SET NombVen = ?, DirecVen = ?, Telefono = ? WHERE CodVend = ?", conn);
+            //Añado los parámetros
+            adapter.UpdateCommand.Parameters.Clear();
+            OleDbParameter pNombre = new OleDbParameter("NombVen", -1);
+            pNombre.OleDbType = OleDbType.VarChar;
+            pNombre.SourceColumn = "NombVen";
+            pNombre.ParameterName = "NombVen";
+            adapter.UpdateCommand.Parameters.Add(pNombre);
+            OleDbParameter pDirec = new OleDbParameter("DirecVen", -1);
+            pNombre.OleDbType = OleDbType.VarChar;
+            pNombre.SourceColumn = "DirecVen";
+            pNombre.ParameterName = "DirecVen";
+            adapter.UpdateCommand.Parameters.Add(pDirec);
+            OleDbParameter pTelefono = new OleDbParameter("Telefono", -1);
+            pNombre.OleDbType = OleDbType.VarChar;
+            pNombre.SourceColumn = "Telefono";
+            pNombre.ParameterName = "Telefono";
+            adapter.UpdateCommand.Parameters.Add(pTelefono);
+            OleDbParameter pCod = new OleDbParameter("CodVend", -1);
+            pNombre.OleDbType = OleDbType.Integer;
+            pNombre.SourceColumn = "CodVend";
+            pNombre.ParameterName = "CodVend";
+            adapter.UpdateCommand.Parameters.Add(pCod);
+            //Inserto el adaptador en el commandBuilder
+            OleDbCommandBuilder cb = new OleDbCommandBuilder(adapter);
+            conn.Open();
+            adapter.Update((DataTable) dataGridView1.DataSource);
+            conn.Close();
+        }
+
+        private void btnAddX_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows[0].Cells[2].Value = dataGridView1.Rows[0].Cells[2].Value.ToString() + "x";
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if(dataGridView1.Rows[0].Cells[2].Value.ToString().Length != 0)
+            {
+                dataGridView1.Rows[0].Cells[2].Value = dataGridView1.Rows[0].Cells[2].Value.ToString().Substring(0, dataGridView1.Rows[0].Cells[2].Value.ToString().Length - 1);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                txtNombre.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                txtSalario.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            }
+        }
+
+        private void btnFila_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int fila = Convert.ToInt32(txtFila.Text.ToString());
+                for(int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    dataGridView1.Rows[i].Selected = false;
+                }
+                dataGridView1.Rows[fila - 1].Selected = true;
+                dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.SelectedRows[0].Index;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Debes introducir un número de fila válido");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+             CambioBuscar se actualiza cada vez que cambia el texto
+            Si ha cambiado, entonces vuelvo a buscar por nombre
+            Y escojo el primer nombre entre todas las filas que salen
+            Si no ha cambiado, entonces paso a la siguiente
+             */
+            if(cambioBuscar)
+            {
+                try
+                {
+                    String nombre = txtNombreBuscar.Text.ToString();
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                    {
+                        if (dataGridView1.Rows[i].Cells[1].Value != null && dataGridView1.Rows[i].Cells[1].Value.ToString().Contains(nombre))
+                        {
+                            dataGridView1.Rows[i].Selected = true;
+                        }
+                        else
+                        {
+                            dataGridView1.Rows[i].Selected = false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    MessageBox.Show(ex.Message);
+                }
+                posicion = 0;
+                dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.SelectedRows[posicion].Index;
+            }
+            else
+            {
+                posicion++;
+                if(dataGridView1.SelectedRows.Count > posicion)
+                {
+                    dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.SelectedRows[posicion].Index;
+                }
+                else
+                {
+                    posicion = 0;
+                    dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.SelectedRows[posicion].Index;
+                }
+            }
+            
+        }
+
+        private void txtNombreBuscar_TextChanged(object sender, EventArgs e)
+        {
+            cambioBuscar = true;
         }
     }
 }
