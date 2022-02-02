33c3443e424bd95c351367455ec04b5f32a8bca3
Unai <unai.garciagi@elorrieta-errekamari.com>
2022-02-02 20:02:15 +0100

    Form 3 empezado

--- Reto_DI/Reto_DI/Form3.Designer.cs
+++ Reto_DI/Reto_DI/Form3.Designer.cs
@@ -29,12 +29,70 @@ namespace Reto_DI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(12, 12);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(100, 23);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(119, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(12, 41);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(100, 23);
+            this.btnBorrar.TabIndex = 2;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(12, 134);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(552, 304);
+            this.reportViewer1.TabIndex = 3;
+            this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 510);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnMostrar);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnBorrar;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
     }
 }
--- Reto_DI/Reto_DI/Form3.cs
+++ Reto_DI/Reto_DI/Form3.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Reporting.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,79 @@ namespace Reto_DI
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private string sql;
+        private MiConexion Conexion = new MiConexion();
+        //SqlCommand cmd = new SqlCommand(sql, Conexion.pConexion);
+        //Conexion.AbrirConexion();
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            rellenarCombo();
+            sql = "SELECT CodFac, NombCli, Importe, Fecha FROM Facturas f JOIN Clientes c ON f.CodCli = c.CodCli";
+            SqlCommand cmd = new SqlCommand(sql, Conexion.pConexion);
+            SqlDataAdapter das = new SqlDataAdapter(cmd);
+            Conexion.AbrirConexion();
+            DataSet ds = new DataSet();
+            das.Fill(ds, "tabla");
+            ReportDataSource rds = new ReportDataSource();
+            rds.Name = "Facturas";
+            rds.Value = ds.Tables[0];
+            reportViewer1.LocalReport.DataSources.Add(rds);
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedItem != null)
+            {
+                sql = "SELECT SUM(Importe) FROM Facturas WHERE CodCli = " + Convert.ToInt32(comboBox1.SelectedItem.ToString());
+                SqlCommand cmd = new SqlCommand(sql, Conexion.pConexion);
+                Conexion.AbrirConexion();
+                SqlDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+                MessageBox.Show(Convert.ToString(dr.GetDouble(0)));
+                Conexion.CerrarConexion();
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                sql = "DELETE FROM Facturas WHERE CodCli = " + Convert.ToInt32(comboBox1.SelectedItem.ToString());
+                SqlCommand cmd = new SqlCommand(sql, Conexion.pConexion);
+                Conexion.AbrirConexion();
+                cmd.ExecuteNonQuery();
+                Conexion.CerrarConexion();
+                sql = "DELETE FROM Clientes WHERE CodCli = " + Convert.ToInt32(comboBox1.SelectedItem.ToString());
+                cmd = new SqlCommand(sql, Conexion.pConexion);
+                Conexion.AbrirConexion();
+                cmd.ExecuteNonQuery();
+                Conexion.CerrarConexion();
+                rellenarCombo();
+            }
+        }
+
+        private void rellenarCombo()
+        {
+            comboBox1.Items.Clear();
+            sql = "SELECT CodCli FROM Clientes ORDER BY CodCli ASC";
+            SqlCommand cmd = new SqlCommand(sql, Conexion.pConexion);
+            Conexion.AbrirConexion();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox1.Items.Add(Convert.ToString(dr.GetInt32(0)));
+            }
+            Conexion.CerrarConexion();
+            comboBox1.SelectedItem = 1;
+        }
+
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
