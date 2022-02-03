5081c03027c4504d929007a6ade5573483cd20e2
Unai <unai.garciagi@elorrieta-errekamari.com>
2022-02-03 18:33:52 +0100

    ComboBox para elegir la demografía ComboBox para elegir el dato Botón para generar el XML

--- DI_Extra_CDC/DI_Extra_CDC/Form1.Designer.cs
+++ DI_Extra_CDC/DI_Extra_CDC/Form1.Designer.cs
@@ -31,15 +31,18 @@ namespace DI_Extra_CDC
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btnXml = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 68);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 93);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(776, 370);
+            this.dataGridView1.Size = new System.Drawing.Size(776, 345);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
@@ -51,11 +54,42 @@ namespace DI_Extra_CDC
             this.label1.TabIndex = 1;
             this.label1.Text = "Vacunaciones USA 2022";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(164, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(346, 17);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(255, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // btnXml
+            // 
+            this.btnXml.Location = new System.Drawing.Point(608, 14);
+            this.btnXml.Name = "btnXml";
+            this.btnXml.Size = new System.Drawing.Size(180, 23);
+            this.btnXml.TabIndex = 4;
+            this.btnXml.Text = "GENERAR XML";
+            this.btnXml.UseVisualStyleBackColor = true;
+            this.btnXml.Click += new System.EventHandler(this.btnXml_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnXml);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -71,6 +105,9 @@ namespace DI_Extra_CDC
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btnXml;
     }
 }
 
--- DI_Extra_CDC/DI_Extra_CDC/Form1.cs
+++ DI_Extra_CDC/DI_Extra_CDC/Form1.cs
@@ -33,22 +33,95 @@ namespace DI_Extra_CDC
                 Console.WriteLine(ex.Message);
                 MessageBox.Show("No se ha podido descargar, se utilizará el dato en local");
             }
+            cargarFilas();
+            cargarColumnas();
+            cargarJson("US", 5);
+        }
+
+        private void cargarJson(String campo, int columna)
+        {
             string filejson = File.ReadAllText(@"datos.json");
             //Coge todo lo que hay en el string como texto plano, ignorando caracteres escape y otros simbolos
             DataTable dt = (DataTable)JsonConvert.DeserializeObject(filejson, typeof(DataTable));
             List<int> posiciones = new List<int>();
-            for(int i = 0; i < dt.Rows.Count; i++)
+            for (int i = 0; i < dt.Rows.Count; i++)
             {
-                if (!dt.Rows[i].ItemArray[1].ToString().Equals("US"))
+                if (!dt.Rows[i].ItemArray[1].ToString().Equals(campo))
                 {
                     posiciones.Add(i);
                 }
             }
-            for(int i = posiciones.Count - 1; i >= 0; i--)
+            for (int i = posiciones.Count - 1; i >= 0; i--)
             {
                 dt.Rows[(int)posiciones[i]].Delete();
             }
+            for(int i = dt.Columns.Count - 1; i >= 0; i--)
+            {
+                if(i != 0 && i != 1 && i != columna)
+                {
+                    dt.Columns.RemoveAt(i);
+                }
+            }
             dataGridView1.DataSource = dt;
+            dataGridView1.Columns[0].Width = 120;
+            dataGridView1.Columns[1].Width = 200;
+            dataGridView1.Columns[2].Width = 200;
+        }
+
+        private void cargarFilas()
+        {
+            string filejson = File.ReadAllText(@"datos.json");
+            //Coge todo lo que hay en el string como texto plano, ignorando caracteres escape y otros simbolos
+            DataTable dt = (DataTable)JsonConvert.DeserializeObject(filejson, typeof(DataTable));
+            String fecha1 = dt.Rows[0].ItemArray[0].ToString();
+            comboBox1.Items.Add(dt.Rows[0].ItemArray[1].ToString());
+            for(int i = 1; i < dt.Rows.Count && dt.Rows[i].ItemArray[0].ToString().Equals(fecha1); i++)
+            {
+                comboBox1.Items.Add(dt.Rows[i].ItemArray[1].ToString());
+            }
+        }
+
+        private void cargarColumnas()
+        {
+            string filejson = File.ReadAllText(@"datos.json");
+            //Coge todo lo que hay en el string como texto plano, ignorando caracteres escape y otros simbolos
+            DataTable dt = (DataTable)JsonConvert.DeserializeObject(filejson, typeof(DataTable));
+            for (int i = 2; i < dt.Columns.Count; i++)
+            {
+                comboBox2.Items.Add(dt.Columns[i].ToString());
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox2.SelectedIndex == -1)
+            {
+                cargarJson(comboBox1.Text.ToString(), 5);
+            }
+            else
+            {
+                cargarJson(comboBox1.Text.ToString(), comboBox2.SelectedIndex + 2);
+            }
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                cargarJson("US", comboBox2.SelectedIndex + 2);
+            }
+            else
+            {
+                cargarJson(comboBox1.Text.ToString(), comboBox2.SelectedIndex + 2);
+            }
+        }
+
+        private void btnXml_Click(object sender, EventArgs e)
+        {
+            DataTable dt = (DataTable) dataGridView1.DataSource;
+            dt.TableName = "datos";
+            dt.WriteXml("datos.xml");
         }
     }
 }
