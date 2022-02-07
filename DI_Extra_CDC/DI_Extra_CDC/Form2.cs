80712f7ec7b3887d174e7edd511fac33ef4ecabb
Unai García Giralde <unaibaraka@outlook.es>
2022-02-07 09:32:14 +0100

    Se crea un fichero xlsx y se abre con excel

--- DI_Extra_CDC/DI_Extra_CDC/Form2.cs
+++ DI_Extra_CDC/DI_Extra_CDC/Form2.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -49,6 +51,68 @@ namespace DI_Extra_CDC
                 //Lanzo el proceso con el enlace
                 System.Diagnostics.Process.Start("https://www.google.com/maps/search/?api=1&query=" + latitud+","+longitud);
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dt = (DataTable)dataGridView1.DataSource;
+            /*
+            StreamWriter sw = new StreamWriter("datos.csv", false);
+            //headers    
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                sw.Write(dt.Columns[i]);
+                if (i < dt.Columns.Count - 1)
+                {
+                    //Separador
+                    sw.Write(",");
+                }
+            }
+            sw.Write(sw.NewLine);
+            foreach (DataRow dr in dt.Rows)
+            {
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    if (!Convert.IsDBNull(dr[i]))
+                    {
+                        string value = dr[i].ToString();
+                        //Se asegura que el separador no reaparezca
+                        if (value.Contains(','))
+                        {
+                            value = String.Format("\"{0}\"", value);
+                            sw.Write(value);
+                        }
+                        else
+                        {
+                            sw.Write(dr[i].ToString());
+                        }
+                    }
+                    if (i < dt.Columns.Count - 1)
+                    {
+                        //Separador
+                        sw.Write(",");
+                    }
+                }
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
+            System.Diagnostics.Process.Start("datos.csv");
+            */
+            //PARA ESTO NECESITAMOS EL PAQUETE NUGGET EPPLUS VERSIÓN 4.X
+            //USAMOS LA 4.X PORQUE NO HE SIDO CAPAZ DE HACER FUNCIONAR LA 5.X CON EL CAMBIO DE LICENCIA
+            //Creo un excel
+            ExcelPackage ep = new ExcelPackage();
+            //Añado la primera hoja
+            ExcelWorksheet ew = ep.Workbook.Worksheets.Add("Sheet 1");
+            //Le cargo la tabla en la celda A1
+            ew.Cells["A1"].LoadFromDataTable(dt, true);
+            //Hago que las columnas se autorredimensionen
+            ew.Cells[ew.Dimension.Address].AutoFitColumns();
+            //Guardar como, se crea un fileinfo y luego se guarda
+            FileInfo fi = new FileInfo("datos.xlsx");
+            ep.SaveAs(fi);
+            //Inicio excel
+            System.Diagnostics.Process.Start("datos.xlsx");
         }
     }
 }
